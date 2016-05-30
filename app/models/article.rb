a066a4848dde5a46f270eb3fb849c68da5108b21
Dmitry Polyakovsky <dmitrypol@gmail.com>
2016-05-30 07:50:56 -0700

    tweaked article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,13 @@
 class Article < ActiveRecord::Base
 
-  belongs_to :user, counter_cache: true
+  include Redis::Objects
+  value :redobj_attr, marshal: true, expiration: 1.hour
+
+  belongs_to :user, counter_cache: true, touch: true
   validates :title, :body, :user, presence: true
 
   after_create do 			user.redobj_articles_count.incr	 	end
 	after_destroy do 			user.redobj_articles_count.decr	 	end
+  after_save do					self.redobj_attr = self.attributes 	end
 
 end
